ce8e37e05a044c8243e37c769fb6595b3a6149b4
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-04-11 16:23:42 -0500

    Implementar clase Quiz y funcion principal del juego
    En trivia.py, se añadió la clase Quiz que representa las preguntas
    de la trivia y la función run_quiz, que se encargará de generar las
    preguntas y el bucle central del juego.
    
    Tanto en trivia.py y test_trivia.py se añadieron comentarios a
    modo de documentación.

--- trivia-game-python/app/test_trivia.py
+++ trivia-game-python/app/test_trivia.py
@@ -1,6 +1,7 @@
 import pytest
 from trivia import Question
 
+# Pruebas para la clase Question
 def test_question_correct_answer():
     question = Question("What is 2 + 2?", ["1", "2", "3", "4"], "4")
     assert question.is_correct("4")
--- trivia-game-python/app/trivia.py
+++ trivia-game-python/app/trivia.py
@@ -1,3 +1,4 @@
+# Clase que representa una pregunta de trivia
 class Question:
     def __init__(self, description, options, correct_answer):
         self.description = description
@@ -6,3 +7,38 @@ class Question:
 
     def is_correct(self, answer):
         return self.correct_answer == answer
+
+# Clase que representa un juego de trivia
+class Quiz:
+    def __init__(self):
+        self.questions = []
+        self.current_question_index = 0
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def get_next_question(self):
+        if self.current_question_index < len(self.questions):
+            question = self.questions[self.current_question_index]
+            self.current_question_index += 1
+            return question
+        return None
+    
+# Funcion principal de ejecucion del juego
+def run_quiz ():
+
+    quiz = Quiz()
+
+    # Establecer preguntas del juego
+    quiz.add_question(Question("Cuanto es 2 + 2", ["1", "2", "3", "4"], "4"))
+    quiz.add_question(Question("Cuanto es 2 + 3", ["2", "3", "4", "5"], "5"))
+    quiz.add_question(Question("Cuanto es 2 + 4", ["2", "3", "4", "6"], "6"))
+    quiz.add_question(Question("Cuanto es 2 + 5", ["2", "3", "4", "7"], "7"))
+
+    # Imprimir preguntas
+    question = quiz.get_next_question()
+    while question:
+        print(f"Pregunta {quiz.current_question_index}: {question.description}")
+        question = quiz.get_next_question()
+
+run_quiz()
