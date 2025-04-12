dd080bf2dca60b51524f5ce8f3857090fe988345
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-04-12 12:06:25 -0500

    Añadir prueba para el sistema de puntuación

--- trivia-game-python/app/test_trivia.py
+++ trivia-game-python/app/test_trivia.py
@@ -1,5 +1,5 @@
 import pytest
-from trivia import Question
+from trivia import Quiz, Question
 
 # Pruebas para la clase Question
 def test_question_correct_answer():
@@ -9,3 +9,11 @@ def test_question_correct_answer():
 def test_question_incorrect_answer():
     question = Question("What is 2 + 2?", ["1", "2", "3", "4"], "4")
     assert not question.is_correct("2")
+
+# Pruebas para la clase Quiz
+def test_quiz_scoring():
+    quiz = Quiz ()
+    question = Question ("What is 2 + 2?", ["1", "2", "3", "4"], "4")
+    quiz.add_question(question)
+    assert quiz.answer_question("4") == True
+    assert quiz.correct_answers == 1
