491facc7809ef5979597873bc3fa62314fba0dc3
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-04-12 12:01:01 -0500

    Implementación de sistema de puntuación, manejo de rondas y finalización del juego
    Se implementaron nuevos métodos y atributos de la clase Quiz, relacionados con el
    sistema de puntuación. Además, se modificó el bucle principal del juego, en la
    función `run_quiz()`, para que solo hayan 10 rondas.

--- trivia-game-python/app/trivia.py
+++ trivia-game-python/app/trivia.py
@@ -13,6 +13,8 @@ class Quiz:
     def __init__(self):
         self.questions = []
         self.current_question_index = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
 
     def add_question(self, question):
         self.questions.append(question)
@@ -24,6 +26,14 @@ class Quiz:
             return question
         return None
     
+    def answer_question (self, question, answer):
+        if question.is_correct(answer):
+            self.correct_answers += 1
+            return True
+        else:
+            self.incorrect_answers += 1
+            return False
+    
 # Funcion principal de ejecucion del juego
 def run_quiz ():
 
@@ -34,11 +44,16 @@ def run_quiz ():
     quiz.add_question(Question("Cuanto es 2 + 3", ["2", "3", "4", "5"], "5"))
     quiz.add_question(Question("Cuanto es 2 + 4", ["2", "3", "4", "6"], "6"))
     quiz.add_question(Question("Cuanto es 2 + 5", ["2", "3", "4", "7"], "7"))
+    quiz.add_question(Question("Cuanto es 2 + 6", ["2", "3", "4", "8"], "8"))
+    quiz.add_question(Question("Cuanto es 2 + 7", ["2", "3", "4", "9"], "9"))
+    quiz.add_question(Question("Cuanto es 2 + 8", ["2", "3", "4", "10"], "10"))
+    quiz.add_question(Question("Cuanto es 2 + 9", ["2", "3", "4", "11"], "11"))
+    quiz.add_question(Question("Cuanto es 2 + 10", ["2", "3", "4", "12"], "12"))
+    quiz.add_question(Question("Cuanto es 2 + 11", ["2", "3", "4", "13"], "13"))
 
-    # Imprimir preguntas
-    question = quiz.get_next_question()
-    while question:
-        print(f"Pregunta {quiz.current_question_index}: {question.description}")
+    # Bucle principal del juego
+    while quiz.current_question_index < 10:
         question = quiz.get_next_question()
+        print(f"Pregunta {quiz.current_question_index}: {question.description}")
 
 run_quiz()
