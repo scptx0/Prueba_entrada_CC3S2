047c2fcc0ac752f2a33f30771a798b74723c76ef
scptx0 <134897241+scptx0@users.noreply.github.com>
2025-04-15 21:42:30 -0500

    Modificar main.py para cargar variables

--- trivia-game-python/app/main.py
+++ trivia-game-python/app/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 # Creamos una instancia de FastAPI
 app = FastAPI()
